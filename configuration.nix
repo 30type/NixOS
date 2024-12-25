3f007a2045044cf277b7f7cffdd22253800f7b35
30type <192151853+30type@users.noreply.github.com>
2024-12-25 23:54:38 +0000

    added powertop to manager abysmal battery life. disabled gnome core utils for the same reason

--- configuration.nix
+++ configuration.nix
@@ -92,6 +92,9 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  # Disable gnome core-utils
+  services.gnome.core-utilities.enable = false;
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
@@ -101,6 +104,7 @@
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   wget
   git
+  powertop 
   kitty
   vesktop
   vimPlugins.catppuccin-vim
