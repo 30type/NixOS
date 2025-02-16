2c60d1e70ed2830b42f18dbd77d0a9b9d588a2d3
30type <192151853+30type@users.noreply.github.com>
2025-02-16 14:55:09 +0000

    bundled modules

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -12,19 +12,7 @@
 
 {
   imports = [
-    ../modules/audio.nix
-    ../modules/bluetooth.nix
-    ../modules/bootloader.nix
-    ../modules/emacs.nix
-    ../modules/fish.nix
-    ../modules/fonts.nix
-    ../modules/foot.nix
-    ../modules/locale.nix
-    ../modules/networking.nix
-    ../modules/niri.nix
-    ../modules/nix.nix
-    ../modules/qmk.nix
-    ../modules/users.nix
+    ../modules/
     ./hardware-configuration.nix
   ];
 
---
+++ hosts/modules/default.nix
@@ -0,0 +1,18 @@
+{ ... }:
+{
+  imports = [
+    ../modules/audio.nix
+    ../modules/bluetooth.nix
+    ../modules/bootloader.nix
+    ../modules/emacs.nix
+    ../modules/fish.nix
+    ../modules/fonts.nix
+    ../modules/foot.nix
+    ../modules/locale.nix
+    ../modules/networking.nix
+    ../modules/niri.nix
+    ../modules/nix.nix
+    ../modules/qmk.nix
+    ../modules/users.nix
+  ];
+}
