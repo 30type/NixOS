ad35376e301121f9167411ee54553251920b8b0b
30type <192151853+30type@users.noreply.github.com>
2025-02-28 17:15:29 +0000

    ,

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -1,19 +1,20 @@
 { lib, ... }:
 {
   imports = [
-    ../modules/alacritty.nix
-    ../modules/audio.nix
-    ../modules/bluetooth.nix
-    ../modules/dm.nix
-    ../modules/bootloader.nix
-    ../modules/fish.nix
-    ../modules/fonts
-    ../modules/locale.nix
-    ../modules/networking.nix
-    ../modules/niri.nix
-    ../modules/nix.nix
-    ../modules/packages.nix
-    ../modules/qmk.nix
-    ../modules/users.nix
+    ./alacritty.nix
+    ./audio.nix
+    ./bluetooth.nix
+    ./dm.nix
+    ./bootloader.nix
+    ./fish.nix
+    ./fonts
+    ./locale.nix
+    ./networking.nix
+    ./niri.nix
+    ./nix.nix
+    ./packages.nix
+    ./qmk.nix
+    ./users.nix
+    ./waybar.nix
   ];
 }
