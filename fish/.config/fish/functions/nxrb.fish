e6cfe74c013b142ef023da5e853418a985e1a64d
30type <192151853+30type@users.noreply.github.com>
2025-02-28 14:00:54 +0000

    ,

--- fish/.config/fish/functions/nxrb.fish
+++ fish/.config/fish/functions/nxrb.fish
@@ -1,4 +1,4 @@
-function nxrb --wraps='nixos-rebuild switch --use-remote-sudo --flake ~/.dotfiles/nixos' --description 'alias nxrb nixos-rebuild switch --use-remote-sudo --flake ~/.dotfiles/nixos'
-  nixos-rebuild switch --use-remote-sudo --flake ~/.dotfiles/nixos $argv
+function nxrb --wraps='nixos-rebuild switch --use-remote-sudo --flake ~/.dotfiles/nixos' --wraps='nixos-rebuild switch --flake ~/.dotfiles/nixos' --description 'alias nxrb nixos-rebuild switch --flake ~/.dotfiles/nixos'
+  nixos-rebuild switch --flake ~/.dotfiles/nixos $argv
         
 end
