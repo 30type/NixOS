f8789047a3a085a3838483090dc7acf9686f9e2f
30type <192151853+30type@users.noreply.github.com>
2025-03-03 19:23:51 +0000

    wezterm

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -6,6 +6,10 @@
   ";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugins = {
@@ -25,10 +29,8 @@
     {
       nixosConfigurations.desktop = (
         nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit system;
-            inherit inputs;
-          };
+          specialArgs = { inherit system inputs; };
+
           modules = [
             ./hosts/desktop/configuration.nix
             # inputs.hyprland.nixosModules.default
